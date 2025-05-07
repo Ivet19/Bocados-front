ef8d7dbb6dae0418bef88a0040ae7152ed7503fa
Ivet Lopez <ivet19@live.com>
2025-05-07 20:12:34 +0200

    Update Header and Layout components tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,8 +3,8 @@ import Header from "./Header";
 
 describe("Given the Header component", () => {
   describe("When it renders", () => {
-    test("Then it should show 'OCADOS' inside a level 1 heading", () => {
-      const expectedHeadingText = /ocados/i;
+    test("Then it should show 'BOCADOS' inside a level 1 heading", () => {
+      const expectedHeadingText = /b\s*ocados/i;
 
       render(<Header />);
 
@@ -12,25 +12,6 @@ describe("Given the Header component", () => {
         name: expectedHeadingText,
         level: 1,
       });
-
-      expect(appTitle).toBeInTheDocument();
-    });
-
-    test("Then it should show an image of 'Logo de la aplicación 'Bocados' que representa una B con la silueta de la Sagrada Familia dentro y que tiene un bocado en la parte superior derecha'", () => {
-      const expectedImageAlt =
-        /logo de la aplicación 'bocados' que representa una b con la silueta de la sagrada familia dentro y que tiene un bocado en la parte superior derecha/i;
-
-      render(<Header />);
-
-      const titleImage = screen.getByAltText(expectedImageAlt);
-
-      expect(titleImage).toBeInTheDocument();
-    });
-
-    test("Then it should show the full title 'BOCADOS'", () => {
-      render(<Header />);
-
-      const appTitle = screen.getByLabelText(/bocados/i);
 
       expect(appTitle).toBeInTheDocument();
     });
