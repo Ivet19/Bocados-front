2b910b560e9ef508991d142948d4aab4873de332
Ivet Lopez <ivet19@live.com>
2025-05-07 15:14:03 +0200

    Create tests of Layout component

---
+++ src/components/Layout/Layout.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import Layout from "./Layout";
+
+describe("Given the LAyout component", () => {
+  describe("When it renders", () => {
+    test("Then it should show 'OCADOS' inside a level 1 heading", () => {
+      const expectedHeadingText = /ocados/i;
+
+      render(<Layout />);
+
+      const appTitle = screen.getByRole("heading", {
+        name: expectedHeadingText,
+        level: 1,
+      });
+
+      expect(appTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show an image of 'Logo de la aplicación 'Bocados' que representa una B con la silueta de la Sagrada Familia dentro y que tiene un bocado en la parte superior derecha'", () => {
+      const expectedImageAlt =
+        /logo de la aplicación 'bocados' que representa una b con la silueta de la sagrada familia dentro y que tiene un bocado en la parte superior derecha/i;
+
+      render(<Layout />);
+
+      const titleImage = screen.getByAltText(expectedImageAlt);
+
+      expect(titleImage).toBeInTheDocument();
+    });
+
+    test("Then it should show the full title 'BOCADOS'", () => {
+      render(<Layout />);
+
+      const appTitle = screen.getByLabelText(/bocados/i);
+
+      expect(appTitle).toBeInTheDocument();
+    });
+  });
+});
