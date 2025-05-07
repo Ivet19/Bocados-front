ea15b75ef588ba3db46028b503dff0fd35592b7d
Ivet Lopez <ivet19@live.com>
2025-05-07 20:12:03 +0200

    Add title image inside the heading and ajgust styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,17 @@ const Header: React.FC = () => {
   return (
     <header className="main-header">
       <div className="page-title-container" aria-label="Bocados">
-        <img
-          className="page-title__image"
-          src="logo.svg"
-          alt="Logo de la aplicación 'Bocados' que representa una B con la silueta de la Sagrada Familia dentro y que tiene un bocado en la parte superior derecha"
-          width={60}
-          height={70}
-        />
-        <h1 className="page-title">OCADOS</h1>
+        <h1 className="page-title">
+          {" "}
+          <img
+            className="page-title__image"
+            src="logo.svg"
+            alt="B"
+            width={60}
+            height={70}
+          />
+          OCADOS
+        </h1>
       </div>
     </header>
   );
