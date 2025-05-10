1d52afca0a50ec47329991b081febb3fec9b3f60
Ivet Lopez <ivet19@live.com>
2025-05-10 15:50:21 +0200

    Create Navigation tests and add MemoryRouter wrapper in Layout tests

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,13 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Layout from "./Layout";
 import "./Layout.css";
+import { MemoryRouter } from "react-router";
 
 describe("Given the Layout component", () => {
   describe("When it renders", () => {
     test("Then it should show 'BOCADOS' inside a level 1 heading", () => {
       const expectedHeadingText = /b\s*ocados/i;
 
-      render(<Layout />);
+      render(<Layout />, { wrapper: MemoryRouter });
 
       const appTitle = screen.getByRole("heading", {
         name: expectedHeadingText,
@@ -15,6 +16,16 @@ describe("Given the Layout component", () => {
       });
 
       expect(appTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'Restaurants' link", () => {
+      render(<Layout />, { wrapper: MemoryRouter });
+
+      const restaurantsLink = screen.getByRole("link", {
+        name: /restaurants/i,
+      });
+
+      expect(restaurantsLink).toBeVisible();
     });
   });
 });
