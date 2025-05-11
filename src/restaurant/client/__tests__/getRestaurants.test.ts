2c40453d0fc40e3be79cfbacab47e5c409be6410
Ivet Lopez <ivet19@live.com>
2025-05-11 18:22:45 +0200

    Add getRestaurants function tests

---
+++ src/restaurant/client/__tests__/getRestaurants.test.ts
@@ -0,0 +1,50 @@
+import { http, HttpResponse } from "msw";
+import { server } from "../../mocks/node";
+import { moviesRestaurantsFirstPageDto } from "../../dto/fixturesDto";
+import { mapRestaurantsDtoToRestaurants } from "../../dto/mappers";
+import RestaurantsClient from "../RestaurantClient";
+
+describe("Given the getPosts method of PostClient", () => {
+  describe("When it's called", () => {
+    test("Then it should return Jack Rabbit Slim's, Gusteau's, The Leaky Cauldron, Pizza Planet, Kelp Shake and Moe's Tavern restaurants", async () => {
+      const restaurantClient = new RestaurantsClient();
+
+      const restaurantsData = await restaurantClient.getRestaurants();
+
+      const restaurants = restaurantsData.restaurants;
+
+      expect(restaurants).toStrictEqual(
+        mapRestaurantsDtoToRestaurants(moviesRestaurantsFirstPageDto),
+      );
+    });
+
+    test("Then it should return 6 to as the total number of restaurants", async () => {
+      const restaurantsClient = new RestaurantsClient();
+
+      const restaurantsData = await restaurantsClient.getRestaurants();
+
+      const restaurants = restaurantsData.restaurants;
+      const restaurantsTotal = restaurantsData.restaurantsTotal;
+
+      expect(restaurantsTotal).toBe(restaurants.length);
+    });
+  });
+
+  describe("When it's called and response is not ok", () => {
+    test("Then it should throw 'Error fetching restaurants'", () => {
+      const apiUrl = import.meta.env.VITE_API_URL;
+
+      server.use(
+        http.get(`${apiUrl}/restaurants`, () => {
+          return new HttpResponse(null, { status: 500 });
+        }),
+      );
+
+      const restaurantClient = new RestaurantsClient();
+
+      const restaurants = restaurantClient.getRestaurants();
+
+      expect(restaurants).rejects.toThrow("Error fetching restaurants");
+    });
+  });
+});
