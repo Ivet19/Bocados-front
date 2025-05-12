eda17b562f25ac9659bc7c5f7321b2d8789abbb7
Ivet Lopez <ivet19@live.com>
2025-05-12 13:29:05 +0200

    Create RestaurantsPage component with styles and update general styles

---
+++ src/restaurant/pages/RestaurantsPage/RestaurantsPage.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import useRestaurants from "../../hooks/useRestaurants";
+import { useSearchParams } from "react-router";
+import RestaurantsList from "../../components/RestaurantsList/RestaurantsList";
+import "./RestaurantsPage.css";
+
+const RestaurantsPage: React.FC = () => {
+  const { loadRestaurants, restaurantsData } = useRestaurants();
+  const { restaurants, restaurantsTotal } = restaurantsData;
+
+  const [urlSearchParams] = useSearchParams();
+
+  const pageNumber = urlSearchParams.get("page")
+    ? Number(urlSearchParams.get("page"))
+    : 1;
+
+  useEffect(() => {
+    loadRestaurants(pageNumber);
+  }, [loadRestaurants, pageNumber]);
+
+  return (
+    <>
+      <header className="page-header">
+        <h2 className="page-header__title"> Lista de restaurantes</h2>
+        <span className="page-header__counter">
+          {`Total: ${restaurants.length} / ${restaurantsTotal}`}
+        </span>
+      </header>
+      <RestaurantsList restaurants={restaurants} />
+    </>
+  );
+};
+
+export default RestaurantsPage;
