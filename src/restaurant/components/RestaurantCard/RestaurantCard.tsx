4188dfd6774a7d0facf0f27b010289a3355623a6
Ivet Lopez <ivet19@live.com>
2025-05-12 16:56:41 +0200

    Create RestaurantCard component with styles

---
+++ src/restaurant/components/RestaurantCard/RestaurantCard.tsx
@@ -0,0 +1,68 @@
+import type { Restaurant } from "../../types";
+import "./RestaurantCard.css";
+
+interface RestaurantCardProps {
+  restaurant: Restaurant;
+}
+
+const RestaurantCard: React.FC<RestaurantCardProps> = ({
+  restaurant: { name, adress, foodType, imageUrl, imageAlt, isVisited, rating },
+}) => {
+  const checkIcon = isVisited
+    ? "/icons/Visited-green.svg"
+    : "/icons/Visited-grey.svg";
+
+  const checkIconColor = isVisited ? "green" : "grey";
+
+  return (
+    <article className="restaurant">
+      <div className="restaurant__top-info">
+        <h3 className="restaurant__name">{name}</h3>
+        <img src={checkIcon} alt={`${checkIconColor} check icon`} />
+      </div>
+      <img
+        className="restaurant__image"
+        src={imageUrl}
+        alt={imageAlt}
+        width={319}
+        height={180}
+      />
+      <div className="restaurant__bottom-info">
+        <div className="restaurant__info">
+          <img
+            className="restaurant__location-icon"
+            src="/icons/Location-icon.svg"
+            alt="location map icon"
+            width={20}
+            height={20}
+          />
+          <span>{adress}</span>
+        </div>
+        <div className="restaurant__info">
+          <img
+            className="restaurant__food-icon"
+            src="/icons/Food-icon.svg"
+            alt="crossed knife and spoon icon"
+            width={20}
+            height={20}
+          />
+          <span>{foodType}</span>
+        </div>
+        {rating && (
+          <div className="restaurant__info">
+            <img
+              className="restaurant__star-icon"
+              src="/icons/Star-icon.svg"
+              alt="gold star icon"
+              width={20}
+              height={20}
+            />
+            <span>{`${rating}/5`}</span>
+          </div>
+        )}
+      </div>
+    </article>
+  );
+};
+
+export default RestaurantCard;
