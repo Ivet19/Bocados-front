22a1491ce2c605e13b762789b23c217aa3df9fa6
Ivet Lopez <ivet19@live.com>
2025-05-13 13:33:54 +0200

    Add test to RestaurantsPage tests

--- src/restaurant/pages/RestaurantsPage/RestaurantsPage.test.tsx
+++ src/restaurant/pages/RestaurantsPage/RestaurantsPage.test.tsx
@@ -3,6 +3,13 @@ import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router";
 import RestaurantsPage from "./RestaurantsPage";
 import store from "../../../store/store";
+import {
+  gusteaus,
+  jackRabbitSlims,
+  kelpShake,
+  leakyCauldron,
+  pizzaPlanet,
+} from "../../fixtures";
 
 describe("Given the RestaurantsPage component", () => {
   describe("When it renders", () => {
@@ -19,6 +26,29 @@ describe("Given the RestaurantsPage component", () => {
       });
 
       expect(pageTitle).toBeVisible();
+    });
+
+    describe("When it renders in /restaurants", () => {
+      test("Then it should show Jack Rabbit Slim's, Gusteau's, The Leaky Cauldron, Pizza Planet and Kelp Shake inside a heading", async () => {
+        const expectedTitles = [
+          jackRabbitSlims.name,
+          gusteaus.name,
+          leakyCauldron.name,
+          pizzaPlanet.name,
+          kelpShake.name,
+        ];
+
+        render(
+          <Provider store={store}>
+            <RestaurantsPage />
+          </Provider>,
+          { wrapper: MemoryRouter },
+        );
+
+        expectedTitles.forEach((title) => {
+          expect(screen.getByText(title)).toBeInTheDocument();
+        });
+      });
     });
   });
 });
