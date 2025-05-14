27778db116509b4db71e50b4c31370484d82f5d7
Ivet Lopez <ivet19@live.com>
2025-05-14 15:23:44 +0200

    Add animation to Loading image

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -6,6 +6,7 @@ const Loading: React.FC = () => {
     <div className="loading">
       <h2 className="loading__message">Cargando información...</h2>
       <img
+        className="loading__image"
         src="images/loading.svg"
         alt="an exhausted man sitting on a table waiting for food"
         width={175}
