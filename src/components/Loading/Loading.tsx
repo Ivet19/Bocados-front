e63cef902f27aaeaae64683e7d094e735ccce117
Ivet Lopez <ivet19@live.com>
2025-05-14 12:13:34 +0200

    Create Loading component with styles

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,19 @@
+import type React from "react";
+import "./Loading.css";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="loading">
+      <h2 className="loading__message">Cargando información...</h2>
+      <img
+        src="images/loading.svg"
+        alt="an exhausted man sitting on a table waiting for food"
+        width={175}
+        height={180}
+      />
+      <span className="loading__message">¡Gracias por tu paciencia!</span>
+    </div>
+  );
+};
+
+export default Loading;
