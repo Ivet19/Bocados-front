85a98a038423544ad80f0ce0c19e5409bc46c30c
Ivet Lopez <ivet19@live.com>
2025-05-21 15:51:59 +0200

    Add delete button test from Layout component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
-import AppRouter from "./router/AppRouter";
+import AppRouter from "./router/AppRouter/AppRouter";
 import store from "./store/store";
 import "@fontsource/text-me-one/index.css";
 import "@fontsource-variable/beiruti/index.css";
