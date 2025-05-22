cd6cbc394e4cc7997aab5131d2b160653fb905e7
Ivet Lopez <ivet19@live.com>
2025-05-22 10:47:18 +0200

    Add modal messages in useRestaurants methods and add it in Layout

--- src/restaurant/hooks/useRestaurants.ts
+++ src/restaurant/hooks/useRestaurants.ts
@@ -1,15 +1,20 @@
 import { useCallback, useMemo } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import RestaurantsClient from "../client/RestaurantClient";
+import useLoading from "../../hooks/hooks/useLoading";
 import {
   createRestaurantActionCreator,
   deleteRestaurantActionCreator,
   loadRestaurantsActionCreator,
   updateRestaurantActionCreator,
 } from "../slice/restaurantSlice";
+import useModal from "../../hooks/hooks/useModal";
 import type { RestaurantData } from "../types";
 
 const useRestaurants = () => {
+  const { startLoading, stopLoading } = useLoading();
+  const { showModal } = useModal();
+
   const restaurantsData = useAppSelector(
     (state) => state.restaurantStateData.restaurantsData,
   );
@@ -20,44 +25,77 @@ const useRestaurants = () => {
 
   const loadRestaurants = useCallback(
     async (pageNumber?: number): Promise<void> => {
-      const restaurantsData = await restaurantClient.getRestaurants(pageNumber);
+      const loading = setTimeout(() => {
+        startLoading();
+      }, 200);
 
-      const action = loadRestaurantsActionCreator(restaurantsData);
+      try {
+        const restaurantsData =
+          await restaurantClient.getRestaurants(pageNumber);
 
-      dispatch(action);
+        const action = loadRestaurantsActionCreator(restaurantsData);
+
+        dispatch(action);
+      } catch {
+        showModal(
+          false,
+          "NO SE HA PODIDO OBTENER LA INFORMACIÓN DE LOS RESTAURANTES",
+        );
+      } finally {
+        clearTimeout(loading);
+      }
+
+      stopLoading();
     },
-    [restaurantClient, dispatch],
+    [showModal, startLoading, restaurantClient, stopLoading, dispatch],
   );
 
   const updateRestaurant = useCallback(
     async (restaurantId: string): Promise<void> => {
-      const restaurant =
-        await restaurantClient.toggleRestaurantById(restaurantId);
+      try {
+        const restaurant =
+          await restaurantClient.toggleRestaurantById(restaurantId);
 
-      const action = updateRestaurantActionCreator(restaurant);
+        const action = updateRestaurantActionCreator(restaurant);
 
-      dispatch(action);
+        dispatch(action);
+      } catch {
+        showModal(false, "NO SE HA PODIDO CAMBIAR EL ESTADO DEL RESTAURANTE");
+      }
     },
-    [restaurantClient, dispatch],
+    [showModal, restaurantClient, dispatch],
   );
 
   const createRestaurant = async (
     restaurantData: RestaurantData,
   ): Promise<void> => {
-    const newRestaurant = await restaurantClient.addRestaurant(restaurantData);
+    try {
+      const newRestaurant =
+        await restaurantClient.addRestaurant(restaurantData);
 
-    const action = createRestaurantActionCreator({ newRestaurant });
+      showModal(true, "RESTAURANTE CREADO CORRECTAMENTE");
 
-    dispatch(action);
+      const action = createRestaurantActionCreator({ newRestaurant });
+
+      dispatch(action);
+    } catch {
+      showModal(false, "NO SE HA PODIDO CREAR EL RESTAURANTE CORRECTAMENTE");
+    }
   };
 
   const removeRestaurant = async (restaurantId: string): Promise<void> => {
-    const deletedRestaurant =
-      await restaurantClient.deleteRestaurant(restaurantId);
+    try {
+      const deletedRestaurant =
+        await restaurantClient.deleteRestaurant(restaurantId);
 
-    const action = deleteRestaurantActionCreator({ deletedRestaurant });
+      showModal(true, "RESTAURANTE ELIMINADO CORRECTAMENTE");
 
-    dispatch(action);
+      const action = deleteRestaurantActionCreator({ deletedRestaurant });
+
+      dispatch(action);
+    } catch {
+      showModal(false, "NO SE HA PODIDO ELIMINAR EL RESTAURANTE CORRECTAMENTE");
+    }
   };
 
   return {
