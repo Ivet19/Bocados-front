676b7730ac3370452d3f6c3c1edf72d63cdba141
Ivet Lopez <ivet19@live.com>
2025-05-22 12:07:17 +0200

    Create lazy loaded Pages with lazy function and add them in AppRouter

--- src/router/AppRouter/AppRouter.tsx
+++ src/router/AppRouter/AppRouter.tsx
@@ -1,17 +1,19 @@
 import { Navigate, Route, Routes } from "react-router";
 import App from "../../components/App/App";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import RestaurantsPage from "../../restaurant/pages/RestaurantsPage/RestaurantsPage";
-import AddRestaurantPage from "../../restaurant/pages/AddRestaurantPage/AddRestaurantPage";
+import {
+  LazyAddRestaurantPage,
+  LazyNotfoundPage,
+  LazyRestaurantsPage,
+} from "../LazyPages/LazyPages";
 
 const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Navigate to="/restaurants" />} />
-        <Route path="restaurants" element={<RestaurantsPage />} />
-        <Route path="add-restaurant" element={<AddRestaurantPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="restaurants" element={<LazyRestaurantsPage />} />
+        <Route path="add-restaurant" element={<LazyAddRestaurantPage />} />
+        <Route path="*" element={<LazyNotfoundPage />} />
       </Route>
     </Routes>
   );
